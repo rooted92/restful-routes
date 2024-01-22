abf679a3f2f83063345b16a1a9299cc549ac46b7
Pedro Castaneda <petecast76@gmail.com>
2024-01-22 08:33:04 -0800

    added patch to update comment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+// We extracted the v4 method from uuid and aliased it to uuid. This is the same as writing const uuid = require('uuid').v4;
+const { v4: uuid } = require('uuid');
 
 // urlencoded says, anytime a request comes in with urlencoded data, go ahead and parse it for me. Like from a form.
 app.use(express.urlencoded({ extended: true }));
@@ -11,22 +13,22 @@ app.set('view engine', 'ejs');
 
 const comments = [
     {
-        id: 1,
+        id: uuid(),
         username: 'Todd',
         comment: 'lol that is so funny!'
     },
     {
-        id: 2,
+        id: uuid(),
         username: 'Skyler',
         comment: 'I like to go birdwatching with my dog'
     },
     {
-        id: 3,
+        id: uuid(),
         username: 'Sk8erBoi',
         comment: 'Plz delete your account, Todd'
     },
     {
-        id: 4,
+        id: uuid(),
         username: 'onlysayswoof',
         comment: 'woof woof woof'
     }
@@ -43,15 +45,26 @@ app.get('/comments/new', (req, res) => {
 app.post('/comments', (req, res) => {
     // We destructured username and comment from req.body, and then pushed them into the comments array.
     const { username, comment } = req.body;
-    comments.push({ username, comment });
+    comments.push({ username, comment, id: uuid() });
     // res.redirect will send the user back to the comments page after they submit a comment.
     res.redirect('/comments');
 });
 
 app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
-    const comment = comments.find(c => c.id === parseInt(id));
+    // Remember find gives you the first matching element in an array.
+    const comment = comments.find(c => c.id === id);
     res.render('comments/show', { comment });
+});
+
+// A patch request will update one part of a resource, whereas a put request will replace the entire resource.
+app.patch('/comments/:id', (req, res) => {
+    res.send('PATCH!!!');
+    const { id } = req.params;
+    const foundComment = comments.find(c => c.id === id);
+    const newCommentText = req.body.comment;
+    // We update the found comments' comment property to be the new comment text.
+    foundComment.comment = newCommentText;
 });
 
 app.get('/tacos', (req, res) => {
